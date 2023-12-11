11ac4fc471427bc9387ca5b38d959dc235d53a2c
ravikant122 <41147204+ravikant122@users.noreply.github.com>
2023-12-11 18:05:25 +0530

    Create classOnParentWithMultiChild.vue

---
+++ classOnParentWithMultiChild.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <!-- when parent has multiple childs
+  then style given to parent won't be applicable to all the children
+  so each child needs to use the style by itself
+  -->
+  <p :class="$attrs.class">Hi</p>
+  <span>This is a child component</span>
+  <p>
+    <span :class="$attrs.class">Hi</span>
+  </p>
+</template>
