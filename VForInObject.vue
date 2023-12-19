399035f1bf5af3ee060136fd00d203b47758df0e
ravikant122 <41147204+ravikant122@users.noreply.github.com>
2023-12-19 17:26:16 +0530

    Create VForInObject.vue

---
+++ VForInObject.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+  	return {
+	    myObject: {
+  	    title: 'How to do lists in Vue',
+	      author: 'Jane Doe',
+      	publishedAt: '2016-04-10'
+    	}
+  	}
+	}
+}
+</script>
+
+<template>
+	<ul>
+		<!-- it will only display the values -->
+		<li v-for="value in myObject">
+			{{ value }}
+		</li>
+	</ul>
+	<ul>
+		<li v-for="(value, key) in myObject">
+			{{ key }}: {{ value }}
+		</li>
+	</ul>
+	<ul>
+    <li v-for="(value, key, index) in myObject">
+		  {{ index }}. {{ key }}: {{ value }}
+		</li>
+  </ul>
+</template>
