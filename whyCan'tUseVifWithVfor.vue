426aa1ab4ba93d537ae363b38b1f4487edcace4a
ravikant122 <41147204+ravikant122@users.noreply.github.com>
2023-12-19 17:31:56 +0530

    Create whyCan'tUseVifWithVfor.vue

---
+++ whyCan'tUseVifWithVfor.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+  	return {
+	    todos: [
+				{
+					name: 'ravi',
+					isCompleted: true
+				},
+				{
+					name: 'abhi',
+					isCompleted: false
+				}
+			]
+  	}
+	}
+}
+</script>
+
+<template>
+	<ul>
+		<li v-for="todo in todos" v-if="todo.isCompleted"> <!-- this lines gives error -->
+			{{ todo.name }}
+		</li>
+		<!-- v-if has higher precendence than v-for that's why v-if can't access the variables of v-for -->
+	</ul>
+</template>
