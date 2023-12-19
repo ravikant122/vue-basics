a0c7fd6713b6c4ff4a028fa539b7ee9d268a8e11
ravikant122 <41147204+ravikant122@users.noreply.github.com>
2023-12-19 17:22:25 +0530

    Create indexInVFor.vue

---
+++ indexInVFor.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+  	return {
+    	items: [{ message: 'Foo' }, { message: 'Bar' }]
+  	}
+	}
+}
+</script>
+
+<template>
+	<!-- you can always index, vue will give you that -->
+	<li v-for="(item, index) in items">
+  	{{ index }} - {{ item.message }}
+	</li>
+</template>
