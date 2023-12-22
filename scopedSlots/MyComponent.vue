8c5dedfce706bd9a1581ae36f21072e24a83bc62
unknown <ravikanttatiwal1999@gmail.com>
2023-12-22 16:27:55 +0530

    scoped slots

---
+++ scopedSlots/MyComponent.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      greetingMessage: "hello",
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <slot :text="greetingMessage" :count="1"></slot>
+  </div>
+</template>
---
+++ scopedSlots/parent.vue
@@ -0,0 +1,19 @@
+<script>
+import MyComponent from "./MyComponent.vue";
+
+export default {
+  components: {
+    MyComponent,
+  },
+};
+</script>
+
+<template>
+  <!-- slots can't access childs dat but there's a way - we can use slot props
+    its like we'll give props to the slots in child component and slot is then use the those
+    props in parent component, since the props in slot are being in child it can use childs data
+  -->
+  <MyComponent v-slot="slotPropsss">
+    {{ slotPropsss.text }} -- {{ slotPropsss.count }}
+  </MyComponent>
+</template>
