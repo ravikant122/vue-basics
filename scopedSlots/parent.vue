c3b2f4e523ee5a1b85e0789fc15432fdddb856c9
unknown <ravikanttatiwal1999@gmail.com>
2023-12-22 16:30:32 +0530

    scoped slots - props object

--- scopedSlots/parent.vue
+++ scopedSlots/parent.vue
@@ -12,6 +12,8 @@ export default {
   <!-- slots can't access childs dat but there's a way - we can use slot props
     its like we'll give props to the slots in child component and slot is then use the those
     props in parent component, since the props in slot are being in child it can use childs data
+
+    here slotPropsss = props object given in slot in child component
   -->
   <MyComponent v-slot="slotPropsss">
     {{ slotPropsss.text }} -- {{ slotPropsss.count }}
