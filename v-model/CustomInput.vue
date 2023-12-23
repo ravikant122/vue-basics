b28351fd91ab938fd11e8b66857f2f6df1b16c41
unknown <ravikanttatiwal1999@gmail.com>
2023-12-23 12:29:06 +0530

    v-model basics

---
+++ v-model/CustomInput.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  // we cannot use some other name in this prop becuase v-model has fixed name for its prop that is modelValue
+  props: ['modelValue'],
+  emits: ['update:modelValue']
+}
+</script>
+
+<template>
+  <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
---
+++ v-model/parent.vue
@@ -0,0 +1,21 @@
+<script>
+import CustomInput from './CustomInput.vue'
+
+export default {
+  components: { CustomInput },
+  data() {
+    return {
+      message: 'hello'
+    }
+  }
+}
+</script>
+
+<template>
+<!-- v-model can be expand into
+  :modelValue="message" @update:modelValue="newValue => message = newValue"
+
+  modelValue name is fixed, that's why we've to create a prop of name modelValue
+-->
+  <CustomInput v-model="message" /> {{ message }}
+</template>
